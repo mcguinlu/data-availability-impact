10dbed63a4b24f58cda72a26137f74388873f04c
Luke McGuinness <luke.mcguinness@bristol.ac.uk>
2020-06-19 15:46:46 +0100

    Restructure code

---
+++ R/1_get_data_avail_statements.R
@@ -0,0 +1,74 @@
+# SET-UP ------------------------------------------------------------------
+
+# Library calls
+library(medrxivr)
+library(rvest)
+library(stringr)
+library(dplyr)
+
+
+# GET RECORDS -------------------------------------------------------------
+
+# Limit to those records posted on medRxiv up to & including 1st May 2020
+# Using medRxiv snapshot 2020-05-26 14:16
+# Returns 4101 records
+
+snap_url <- paste0("https://raw.githubusercontent.com/mcguinlu/medrxivr-data/",
+                   "795081b8895faba1868e4978acfc725c456d0449/",
+                   "snapshot.csv") 
+             
+df <- mx_search(data = read.csv(snap_url,
+                                stringsAsFactors = FALSE),
+                query = "*",
+                to.date = 20200501)
+
+# note about excluding records when filtering on date.
+# Need to filter on first publication date
+
+# Generate new empty variable to hold availability statements
+df$data_avail <- character(nrow(df))
+
+
+# SCRAPE DATA AVAILABILITY STATEMENTS -------------------------------------
+
+# Extract availability statements by browsing to the webpage for each record
+for (link in 1:nrow(df)) {
+  message(paste0("Record ",link," of ", nrow(df)))
+  
+  # ?versioned=TRUE ensures that the link resolves to the correct version
+  # As the availability statements don't seem to vary by version, remove this
+  base_url <- gsub("\\?versioned=TRUE","",df$link_page[link])
+  
+  # Download ".external-links" page for the record, allowing for 503 errors
+  try <- 1
+  while (try <= 5) {
+    message(paste0("Try ", try))
+    Sys.sleep(runif(1,10,13))
+    page <- try(read_html(paste0(base_url,".external-links")))
+    if (!is(page, 'try-error')) break
+    try <- try + 1
+  }
+  
+  if (is(page, 'try-error')) next
+  
+  # Capture, clean and coerce text in ".external-links-view" node to single cell
+  df$data_avail[link] <- page %>%
+    html_node(".external-links-view") %>%
+    str_extract_all(., "(?<=\\>)[^<\n]+", ) %>% 
+    .[[1]] %>% 
+    paste(., collapse = " ") %>%
+    gsub("Data Availability ","",.) 
+  
+  # Process takes a while, due to delay between scrapes
+  # Every 100 records, save to a temporary file
+  if (link%%100 == 0) {
+    write.csv(df, "data/data-avail-tmp.csv", row.names = FALSE)
+  }
+  
+}
+
+# Save final dataframe to CSV file
+write.csv(df, "data/data-avail.csv", row.names = FALSE)
+
+
+
---
+++ R/2_create_independent_datasets.R
@@ -0,0 +1,82 @@
+# Set parameters
+
+n_rev <- 2
+n_subset <- 200
+
+
+# PRIMARY DATASET ---------------------------------------------------------
+
+
+
+# P1&2
+# requires whole dataset, but must be blinded to study details
+
+df <- read.csv("data/data-avail.csv", stringsAsFactors = FALSE)
+
+df_blind <- df %>%
+  dplyr::select(ID, data_avail, published) %>%
+  mutate(decision = as.character(""))
+
+for (rev in 1:n_rev) {
+  rio::export(df_blind,
+              paste0("data/raw/reviewer",rev,"/full_blind_reviewer",rev,".xlsx"),
+              row.names = FALSE)
+}
+
+
+# SECONDARY DATASETS ------------------------------------------------------
+# Independent - do not depend on decisions from primary dataset
+
+# S1
+# Does coding of DAS differ between preprint and published?
+df <- read.csv("data/data-avail.csv",
+               stringsAsFactors = FALSE)
+
+# Set seed for reproducibility
+set.seed(42)
+
+df_s1 <- df %>%
+  # Filter to those that were published
+  dplyr::filter(!is.na(published)) %>%
+  mutate(published_decision = as.character(""), 
+         published_url = paste0("https://www.doi.org/", published),
+         tmp = 1:nrow(.)) %>%
+  dplyr::filter(tmp %in% sample(1:nrow(.), n_subset)) %>%
+  select(ID, title, published_url, published_decision)
+
+class(df_s1$published_url) <- "hyperlink"  
+
+for (rev in 1:n_rev) {
+  rio::export(df_s1,
+              paste0("data/raw/reviewer",rev,"/s1_blind_reviewer",rev,".xlsx"),
+              row.names = FALSE)
+}
+
+# S2
+# Code availability
+# For random sample, how many make code available in PDF but do not mention in DAS?
+
+df <- read.csv("data/data-avail.csv",
+               stringsAsFactors = FALSE)
+
+# Set seed for reproducibility
+# Set to different to above, for diversity of records
+set.seed(41)
+
+df_s2 <- df %>%
+  # Filter to those that were published
+  dplyr::filter(!is.na(published)) %>%
+  mutate(code_DAS_decision = as.character(""), 
+         code_PDF_decision = as.character(""), 
+         preprint_pdf = paste0("https://www.medrxiv.org/content/",doi,"v",version,".full.pdf"),
+         tmp = 1:nrow(.)) %>%
+  dplyr::filter(tmp %in% sample(1:nrow(.), n_subset)) %>%
+  select(ID, title, data_avail, preprint_pdf, code_DAS_decision, code_PDF_decision)
+
+class(df_s2$preprint_pdf) <- "hyperlink"  
+
+for (rev in 1:n_rev) {
+  rio::export(df_s2,
+              paste0("data/raw/reviewer",rev,"/s2_blind_reviewer",rev,".xlsx"),
+              row.names = FALSE)
+}
